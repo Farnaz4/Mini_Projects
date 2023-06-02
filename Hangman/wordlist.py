2979635d8b85bb18856c3d9f57166eb5594c7b95
Farnaz <farnazfawadhasan@gmail.com0>
2023-06-02 23:54:30 +0600

    added on 2/6/2023

---
+++ Hangman/wordlist.py
@@ -0,0 +1,3 @@
+word_list = ['advance', 'dart', 'apple', 'banana', 'cat', 'dog', 'elephant', 'fish', 'guitar', 'helicopter', 'igloo', 'jellyfish', 'kangaroo', 'lion', 'monkey', 'notebook', 'octopus', 'pineapple', 'queen', 'rabbit', 'snake', 'turtle', 'unicorn', 'violin', 'whale', 'xylophone', 'yak', 'zebra', 'ant', 'butterfly', 'caterpillar', 'dragonfly', 'eagle', 'flamingo', 'giraffe', 'hummingbird', 'inchworm', 'jellybean', 'koala', 'ladybug', 'mushroom', 'narwhal', 'ostrich', 'parrot', 'quokka', 'raccoon', 'seahorse', 'toucan', 'umbrella', 'vulture', 'walrus', 'x-ray']
+
+
